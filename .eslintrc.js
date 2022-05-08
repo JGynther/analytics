31e3d62b7b7fe2a8c3309911017875f1af3a2992
Joona Gynther <joona@gynther.xyz>
2022-05-08 16:46:16 +0300

    feat: configure and create first tracker boilerplate

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,15 @@ module.exports = {
   root: true,
   parser: "@typescript-eslint/parser",
   plugins: ["@typescript-eslint"],
-  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "airbnb-base",
+    "airbnb-typescript/base",
+  ],
   env: { node: true },
+  parserOptions: {
+    project: ["trackers/*/tsconfig.json"],
+  },
+  ignorePatterns: [".eslintrc.js"],
 };
