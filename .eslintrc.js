1f843d97dcde9e90cfb1757f903278a53171be5b
Joona Gynther <joona@gynther.xyz>
2022-05-14 15:10:09 +0300

    feat: add express-based boilerplate

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,16 @@
 module.exports = {
   root: true,
-  parser: "@typescript-eslint/parser",
-  plugins: ["@typescript-eslint"],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
   extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "airbnb-base",
-    "airbnb-typescript/base",
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'airbnb-base',
+    'airbnb-typescript/base',
   ],
   env: { node: true },
   parserOptions: {
-    project: ["trackers/*/tsconfig.json"],
+    project: ['trackers/*/tsconfig.json', 'collectors/*/tsconfig.json'],
   },
-  ignorePatterns: [".eslintrc.js"],
+  ignorePatterns: ['.eslintrc.js'],
 };
